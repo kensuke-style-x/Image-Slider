74c14a1c6bfe90310ac929113c8808c547c7a4be
kensuke-style-x <66291400+kensuke-style-x@users.noreply.github.com>
2025-01-03 12:21:33 +0900

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,10 @@
 const sliderWrapper = document.querySelector('.slider-wrapper');
 const images = document.querySelectorAll('.slider-wrapper img');
 
+//button 
+const prevButton = document.querySelector('.prev');
+const nextButton = document.querySelector('.next');
+
 let currentIndex = 0;
 
 const showNextSlide = () => {
@@ -9,11 +13,20 @@ const showNextSlide = () => {
   updateSlider();
 };
 
+const showPrevSlide = () => {
+  currentIndex = (currentIndex - 1 + images.length) % images.length;
+  updateSlider();
+};
+
 const updateSlider = () => {
    const offset = -currentIndex * 100;
   sliderWrapper.style.transform = `translateX(${offset}%)`;
 };
+//addEventLisnter
+nextButton.addEventListener('click', showNextSlide);
+prevButton.addEventListener('click', showPrevSlide);
 
-setInterval(showNextSlide, 3000);
+
+// setInterval(showNextSlide, 3000);
 
   
